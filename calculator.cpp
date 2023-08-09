d75a59e745e3ad7236a0f46d1ce9c53a60836bef
Yumilify <101920479+Yumilify@users.noreply.github.com>
2023-08-09 13:05:48 -0700

    Add files via upload

---
+++ calculator.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+
+{
+//ChatGPT method
+    int num1, num2;
+
+    num1 = 67;
+    num2 = 62;
+    int sum2 = num1 + num2;
+    cout << "these numbers add up to " << sum2 << endl;
+
+    //Different Adding Method
+    int number1 = 42;
+    int number2 = 56;
+    int sum = number1 + number2;
+    cout << "these numbers add up to " << sum << endl;
+
+
+    string characterName = "John";
+    int characterAge;
+    characterAge = 35;
+    cout << "There once was a man named " << characterName << endl;
+    cout << "He was " << characterAge << " years old" << endl;
+    cout << "He liked the name "<< characterName << endl;
+    cout << "But did not like being " << characterAge << endl;
+
+    return 0;
+}
